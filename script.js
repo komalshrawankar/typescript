bc720aaa05adf86b3046c088ae77e9be8bee0ea5
komal shrawankar <shrawankarkomal9@gmail.com>
2026-06-16 18:31:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Komal";
+let age = 22;
+let isStudent = true;
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+//# sourceMappingURL=script.js.map
---
+++ script.ts
@@ -0,0 +1,17 @@
+let userName:string ="Komal";
+let age: number = 22;
+let isStudent:boolean = true;
+
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+
+// 2.Array 
+
+let numbers: number[] = [10,20,30,40];
+let fruits: string[] = ["Apple","Mango","Banana"];
+let marks: Array<number>= [85,90,76];
+
+console.log(numbers);
+console.log(fruits);
+console.log(marks);
